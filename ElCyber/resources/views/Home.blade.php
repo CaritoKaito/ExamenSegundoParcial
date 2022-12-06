d2993f00e8aa3cf8a45c852e841974bf4c985ff5
CaritoKaito <caritogamecortes@gmail.com>
2022-12-05 21:55:34 -0600

    Archivo actualizado

--- ElCyber/resources/views/Home.blade.php
+++ ElCyber/resources/views/Home.blade.php
@@ -4,7 +4,7 @@
 <h1 class="mt-4 display-1 text-center">Home</h1>
 <body>
 
-    <img src="/ciber.jpg"/> 
+    <img class="rounded mx-auto d-block" src="/cyber.jpeg" style= "width:1000px; height:1000px"> 
 
 </body>
 @stop 
